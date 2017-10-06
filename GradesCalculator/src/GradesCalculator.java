c00f50d76f7937d4ec17ae5985623404cd8783bb
motim6026 <moti.malka25@gmail.com>
2017-10-06 10:17:09 +0300

    Signed-off-by: motim6026 <moti.malka25@gmail.com>

--- GradesCalculator/src/GradesCalculator.java
+++ GradesCalculator/src/GradesCalculator.java
@@ -4,27 +4,35 @@ public class GradesCalculator {
 
 	public static void main(String[] args) {
 		
+		
+		
 		// set Scanner for get input from user
 		Scanner inputFromUser = new Scanner(System.in);
-
+        
 		// ask user for numbers of students
 		System.out.print("Enter the number of students: ");
-        byte allStudents = inputFromUser.nextByte();
+                byte allStudents = inputFromUser.nextByte();
 
-        //Set Array For Student
+                //Set Array For Student
 		byte[] sumStudent = new byte[allStudents];
 		
 		//Set Location for count student
 		byte Location = 1;
 		
+		// set variable for Outstanding student
+		byte Outstandingstudent = sumStudent[0];
+		
+		// set variable for topGrades student
+		byte topGrades = sumStudent[0];
+
 		//loop for set Grade to all students
 		for (byte i = 0; i < sumStudent.length; i++) {
 
 			//ask user to enter Grade
 			System.out.print("Enter Grades for student #" + Location++ + ": ");
-            byte Grades = inputFromUser.nextByte();
+                        byte Grades = inputFromUser.nextByte();
 
-            //Check if Grade big proper
+                        //Check if Grade big proper
 			if (Grades < 0 || Grades > 100) {
 				
 				//if Grade not proper, ask user to enter Grade again
@@ -39,28 +47,20 @@ public class GradesCalculator {
 			} else {
 				//set sumStudent to Grades
 				sumStudent[i] = Grades;
+				if (sumStudent[i]>topGrades) {
+					topGrades = sumStudent[i];
+					Outstandingstudent = (byte) (i + 1);
+				}
 
 			}
 
 		}
 
-		// set variable for Outstanding student
-		byte Outstandingstudent = sumStudent[0];
 		
-		// set variable for Outstanding student
-		byte topGrades = sumStudent[0];
-
-		// loop to Check the High Grades
-		for (byte i = 0; i < sumStudent.length; i++) {
-			if (sumStudent[i] > topGrades) {
-				topGrades = sumStudent[i];
-				Outstandingstudent = (byte) (i + 1);
-
-			}
-
-		}
-
+	    // set variable for failed student
 		byte studentfailed = sumStudent[0];
+		
+		// set variable for min Grades 
 		byte minGrades = sumStudent[0];
 		
 		// loop to Check the low Grades
@@ -74,12 +74,14 @@ public class GradesCalculator {
 		double sum = 0;
 		double average = 0;
 		
-		//loop to count the all Grades and 	Department all student
+		//loop to count the all Grades and Department all student
 		for (int number : sumStudent) {
 			sum = (double) (sum + number);
 			average = (double) (sum / allStudents);
 		}
 
+		
+		
 		//print to user The average score 
 		System.out.println("\nThe grade of all students is: " + average + '\n');
 		
@@ -87,6 +89,6 @@ public class GradesCalculator {
 		System.out.println("the student number# " + Outstandingstudent + " got the highest grade and it's: " + topGrades + '\n');
 		
 		//print to user the low grade
-		System.out.println("the student number# " + "" + studentfailed + " got the highest grade and it's: " + minGrades + '\n');
+		System.out.println("the student number# " +  studentfailed + " got the highest grade and it's: " + minGrades + '\n');
 	}
 }
